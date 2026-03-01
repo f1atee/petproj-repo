42a841a1eff67be115e358d591e8f98bb5d3b212
flate <flatesergey@gmail.com>
2026-03-01 21:51:40 +0500

    add simple connection

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"petproj/feature1"
 	"petproj/feature2"
+	simpleconnection "petproj/feature_postgres/simple_connection"
 )
 
 func main() {
 	fmt.Println("test main")
 	feature1.Feature1()
 	feature2.Feature2()
+	simpleconnection.CheckConnection()
 }
